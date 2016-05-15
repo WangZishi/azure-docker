da5166725fb93b84926b7db97a75f405110f6478
Wang Zishi <ynh.2@outlook.com>
2016-05-15 19:24:23 +0800

    Update app

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var http_1 = require('http');
-var server = http_1.createServer(function (req, res) {
-    res.end('Hello world from Docker on Azure!');
-});
-server.listen(8080);
-console.log('Server listening on port 8080.');
-//# sourceMappingURL=app.js.map
